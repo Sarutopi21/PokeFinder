7bbfb4a38b88024f191eb27b614ac96c736a9edc
Sarutopi21 <145146494+Sarutopi21@users.noreply.github.com>
2023-12-23 16:37:35 +0000

    Update StaticGenerator3.cpp
    Brute Force by only searching from IVS

--- Source/Core/Gen3/Generators/StaticGenerator3.cpp
+++ Source/Core/Gen3/Generators/StaticGenerator3.cpp
@@ -1,71 +1,39 @@
-/*
- * This file is part of PokéFinder
- * Copyright (C) 2017-2023 by Admiral_Fish, bumba, and EzPzStreamz
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 3
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
-
-#include "StaticGenerator3.hpp"
-#include <Core/Enum/Lead.hpp>
-#include <Core/Enum/Method.hpp>
-#include <Core/Parents/PersonalInfo.hpp>
-#include <Core/Parents/States/State.hpp>
-#include <Core/RNG/LCRNG.hpp>
-#include <Core/Util/Utilities.hpp>
-
-StaticGenerator3::StaticGenerator3(u32 initialAdvances, u32 maxAdvances, u32 delay, Method method, const StaticTemplate &staticTemplate,
-                                   const Profile3 &profile, const StateFilter &filter) :
-    StaticGenerator(initialAdvances, maxAdvances, delay, method, Lead::None, staticTemplate, profile, filter)
+std::vector<GeneratorState> StaticGenerator3::findIVs(u16 iv1, u16 iv2, u16 iv3, u16 iv4, u16 iv5, u16 iv6) const
 {
-}
+    std::vector<GeneratorState> matchingStates;
 
-std::vector<GeneratorState> StaticGenerator3::generate(u32 seed) const
-{
-    std::vector<GeneratorState> states;
     const PersonalInfo *info = staticTemplate.getInfo();
 
-    PokeRNG rng(seed, initialAdvances + delay);
-    for (u32 cnt = 0; cnt <= maxAdvances; cnt++, rng.next())
+    for (u32 tid = 0; tid <= 65535; tid++)
     {
-        PokeRNG go(rng);
-
-        u32 pid = go.nextUShort();
-        pid |= go.nextUShort() << 16;
-
-        u16 iv1 = go.nextUShort();
-        if (method == Method::Method4)
+        for (u32 sid = 0; sid <= 65535; sid++)
         {
-            go.next();
-        }
-        u16 iv2 = go.nextUShort();
+            for (u32 pid = 0; pid <= 4294967295; pid++)
+            {
+                u32 generatedPID = pid;
+                u32 generatedIV1 = generatedPID & 31;
+                u32 generatedIV2 = (generatedPID >> 5) & 31;
+                u32 generatedIV3 = (generatedPID >> 10) & 31;
+                u32 generatedIV4 = (generatedPID >> 15) & 31;
+                u32 generatedIV5 = (generatedPID >> 20) & 31;
+                u32 generatedIV6 = (generatedPID >> 25) & 31;
 
-        std::array<u8, 6> ivs;
-        ivs[0] = iv1 & 31;
-        ivs[1] = (iv1 >> 5) & 31;
-        ivs[2] = (iv1 >> 10) & 31;
-        ivs[3] = (iv2 >> 5) & 31;
-        ivs[4] = (iv2 >> 10) & 31;
-        ivs[5] = iv2 & 31;
+                if (generatedIV1 == iv1 && generatedIV2 == iv2 && generatedIV3 == iv3 &&
+                    generatedIV4 == iv4 && generatedIV5 == iv5 && generatedIV6 == iv6)
+                {
+                    GeneratorState state(0, pid, {iv1, iv2, iv3, iv4, iv5, iv6},
+                                         pid & 1, Utilities::getGender(pid, info),
+                                         staticTemplate.getLevel(), pid % 25,
+                                         Utilities::getShiny(pid, tsv), info);
 
-        GeneratorState state(initialAdvances + cnt, pid, ivs, pid & 1, Utilities::getGender(pid, info), staticTemplate.getLevel(), pid % 25,
-                             Utilities::getShiny(pid, tsv), info);
-        if (filter.compareState(static_cast<const State &>(state)))
-        {
-            states.emplace_back(state);
+                    if (filter.compareState(static_cast<const State &>(state)))
+                    {
+                        matchingStates.emplace_back(state);
+                    }
+                }
+            }
         }
     }
 
-    return states;
+    return matchingStates;
 }
